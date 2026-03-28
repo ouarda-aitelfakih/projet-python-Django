9ef18e32db3731c8431b3be23242f56382643149
ouarda ait el fakih <ouardaaitelfakih@gmail.com>
2026-03-28 12:56:15 +0100

    [FEAT]:add partie 5 admin configuration

--- student/admin.py
+++ student/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Parent, Student
 
-# Register your models here.
+@admin.register(Parent)
+class ParentAdmin(admin.ModelAdmin):
+    list_display = ('father_name', 'mother_name', 'father_mobile', 'mother_mobile')
+    search_fields = ('father_name', 'mother_name', 'father_mobile')
+    list_filter = ('father_name',)
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'student_id', 'gender', 'student_class', 'section')
+    search_fields = ('first_name', 'last_name', 'student_id', 'admission_number')
+    list_filter = ('gender', 'student_class', 'section')
+    readonly_fields = ('student_image',)
