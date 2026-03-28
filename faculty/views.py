be20414c9b7906bcbad128e8d291eabfc6aa1d6b
ouarda ait el fakih <ouardaaitelfakih@gmail.com>
2026-03-28 16:34:02 +0100

    [FEAT]:add partie 6 et partie 7

--- faculty/views.py
+++ faculty/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render 
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
+def index(request):
+    return render(request, 'authentication/login.html')
 
-# Create your views here. 
-def index(request): 
- #return HttpResponse('First test')
- return render(request, 'home/index.html')
+def dashboard(request):
+    return render(request, 'students/student-dashboard.html')
--- school/settings.py
+++ school/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'faculty',   # Notre application ajoutée ici
-    'student' 
+    'student' ,
+    'home_auth',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+AUTH_USER_MODEL = 'home_auth.CustomUser'
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+)
+LOGIN_URL = '/authentication/login/'
+LOGIN_REDIRECT_URL = '/dashboard/'
